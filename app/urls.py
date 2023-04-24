3f151b80c2e35061b07a6283745b309d9cf3552f
vidhen <71409019+vidhen@users.noreply.github.com>
2023-04-24 12:59:49 +0500

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth import views as auth_views
 from .forms import LoginForm, MyPasswordChangeForm, MyPasswordResetForm, MySetPasswordForm
 
@@ -41,3 +42,5 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('paymentdone/', views.payment_done, name='paymentdone'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
